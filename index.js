89f8977a261b42beaf314bcf959824a74dc56d86
Jonathan Alpart <panjialang@gmail.com>
2020-05-11 17:04:13 -0500

    added questions

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const questions = [
 
+const questions = [
+    "What is your Github user name?",
+    "What is the project Repo URL?",
+    "What is your project Title?",
+    "What is your project Description?",
+    "What are your project Installation Instructions?",
+    "What is your project's intended Usage?",
+    "What type of License would you like to assign?",
+    "How can others Contribute?"
 ];
 
 function writeToFile(fileName, data) {
 }
 
 function init() {
-
 }
 
 init();
